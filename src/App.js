97aaab081aafb0a0ef30e6b79e6608b1c8c84e9d
Ashfaq Nabi <ashfaqnabi357@gmail.com>
2023-02-14 00:35:58 +0500

    13 feb ashfaq

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Button from '@mui/material/Button';
-
+import { MyComponent } from './muiStylee/Button';
 function App() {
   return (
     <div className="App">
         <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <Button variant="contained">Contained</Button>
+    <MyComponent variant="contained">Contained</MyComponent>
     </div>
   );
 }
